c8c86dfc823b8af515f8012e324a1930e09f9fb7
Biruk <kassayebiruk0@gmail.com>
2025-04-16 07:12:11 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-black text-white text-lg font-mono ">Tailwind Test</h1>
+    </>
+  );
+}
+
+export default App;
